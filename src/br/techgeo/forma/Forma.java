dda8c44db369d1b89d1677f223516e0dbd6b3ece
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-06 20:59:53 -0300

    mudadição do ArrayList forma e o interface Volume

---
+++ src/br/techgeo/forma/Forma.java
@@ -0,0 +1,6 @@
+package br.techgeo.forma;
+
+public abstract class Forma {
+    //classe comportamental
+    public abstract double calcularArea();
+}
--- src/br/techgeo/main/Main.java
+++ src/br/techgeo/main/Main.java
@@ -2,24 +2,52 @@ package br.techgeo.main;
 
 import br.techgeo.forma.Cilindro;
 import br.techgeo.forma.Circulo;
+import br.techgeo.forma.Forma;
+import br.techgeo.forma.Volume;
 import br.techgeo.ponto.Ponto;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         Ponto ponto;
+        ArrayList<Forma> lista = new ArrayList<>();
 
-        //cilindro
-        System.out.println("Cilindro");
-        ponto = new Ponto(2,-3);
-        Cilindro cilindro = new Cilindro(8,2,ponto);
-        System.out.println("área = " + cilindro.calcularArea());
-        System.out.println("volume = " + cilindro.calcularVolume());
 
-        //circulo
-        System.out.println("Circulo");
-        ponto = new Ponto(-4,7);
-        Circulo circulo = new Circulo(10,ponto);
-        System.out.println("área = "+ circulo.calcularArea());
+        lista.add(new Cilindro(8,2,new Ponto(2,-3)));
+
+
+        lista.add(new Circulo(10,new Ponto(-4,7)));
+
+
+        lista.add(new Cilindro(3,7.5,new Ponto(1,1)));
+
+
+        lista.add(new Circulo(1.5, new Ponto(2,3)));
+
+        // impressão dos dados
+        for (Forma forma : lista){
+            System.out.println(forma);
+            System.out.println();
+        }
+
+        //impressão das áreas
+        System.out.println("Área: ");
+        for (Forma forma : lista){
+            System.out.println(forma.calcularArea());
+            System.out.println();
+        }
+
+        //Impressão do volume
+        System.out.println("Volume: ");
+        for (Forma forma: lista){
+            if (forma instanceof Volume){
+                // forma.calcularVolume
+                System.out.println(((Volume) forma).calcularVolume());
+                System.out.println();
+            }
+        }
 
 
     }
